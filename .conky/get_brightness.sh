e9b792eacb53ba8ca1f6c305fd04b0f8d131e241
eskerda <eskerda@gmail.com>
2013-12-06 09:33:20 +0100

    The conky config is now a repo

--- .conky/get_brightness.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-MAXVALUE=7
-VALUE=`cat /proc/acpi/ibm/brightness | grep "level:" | awk '{print $2}'`
-echo $(( $VALUE * 100 / $MAXVALUE))
--- .conky/get_light.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-VALUE=`cat /proc/acpi/ibm/light | grep "status:" | awk '{print $2}'`
-if [[ $VALUE == "on" ]]
-then
-    echo "3"
-else
-    echo ""
-fi
